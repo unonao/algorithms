f5a1292d5e085a3196dc38d5529576f5d37064b5
unonao <naokiuno634@gmail.com>
2020-01-21 16:02:00 +0900

    make prime_table(Eratosthenes)

---
+++ math_number_theory/prime_table.cpp
@@ -0,0 +1,34 @@
+/* prime_table.cpp
+    エラトステネスの篩で素数かどうかの情報を保存
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+/*  prime_table(n)
+    入力：整数 n
+    出力：nまでの数字が素数か判定したテーブル
+    計算量：O(nloglogn)
+*/
+vector<bool> prime_table(int n) {
+    vector<bool> prime(n + 1, true);
+    if (n >= 0)
+        prime[0] = false;
+    if (n >= 1)
+        prime[1] = false;
+    for (int i = 2; i * i <= n; i++) {
+        if (!prime[i])
+            continue;
+        for (int j = i + i; j <= n; j += i) {
+            prime[j] = false;
+        }
+    }
+    return prime;
+}
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<bool> p_table = prime_table(N);
+    cout << p_table[N] << endl;
+}
