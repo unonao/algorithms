f0a5efd9f4ff8a9bf4068d92a4579f99a17d96b6
unonao <naokiuno634@gmail.com>
2020-01-21 20:09:54 +0900

    make template

---
+++ template.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0; i < (n); i++)
+#define RREP(i, n) for (int i = (n); i >= 0; i--)
+#define FOR(i, m, n) for (int i = (m); i < (n); i++)
+#define ALL(obj) begin(obj), end(obj)
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+const int INF = 2100100100;
+const int MOD = 1e9 + 7;
+
+// 多次元 vector 生成
+template <class T> vector<T> make_vec(size_t a) { return vector<T>(a); }
+template <class T, class... Ts> auto make_vec(size_t a, Ts... ts) {
+    return vector<decltype(make_vec<T>(ts...))>(a, make_vec<T>(ts...));
+}
+template <class T> bool chmax(T &a, const T &b) {
+    if (a < b) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+template <class T> bool chmin(T &a, const T &b) {
+    if (b < a) {
+        a = b;
+        return 1;
+    }
+    return 0;
+}
+
+int main() {
+    // cin.tie(0);
+    // ios::sync_with_stdio(false);
+    int N;
+    cin >> N;
+
+    cout << N << "\n";
+
+    return 0;
+}
