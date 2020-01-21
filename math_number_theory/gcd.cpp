c3617d6ec5c0d2df0c6aa24415e1127c6a290c18
unonao <naokiuno634@gmail.com>
2020-01-21 14:33:45 +0900

    make gcd.cpp

---
+++ math_number_theory/gcd.cpp
@@ -0,0 +1,24 @@
+/* gcd.cpp
+    Euclidの互除法で、最大公約数を求めるアルゴリズム
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/*  gcd(a,b)
+    入力：整数 a, b
+    出力：aとbの最大公約数
+    計算量：O(log(a)) (a>=bのとき)
+*/
+long long gcd(long long a, long long b) {
+    if (b == 0) {
+        return a;
+    } else {
+        return gcd(b, a % b);
+    }
+}
+
+int main() {
+    int d = gcd(111, 30);
+    cout << d << endl;
+}
