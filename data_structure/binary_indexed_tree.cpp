ce7ccefc0fe9623adf27ec1f87c391d14ae7ead8
unonao <naokiuno634@gmail.com>
2020-03-05 11:22:10 +0900

    update and verify BIT

--- data_structure/binary_indexed_tree.cpp
+++ data_structure/binary_indexed_tree.cpp
@@ -1,15 +1,25 @@
 /* binary_indexed_tree.cpp
     BIT を用いて、区間和の更新や計算を高速に行う。
 
+    verified: AOJ DSL_2_B
+        http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_B&lang=jp
+
+
     応用: 転倒数の計算( i<j において a_i > a_j となる数を数える)
         j(0~n-1)について以下を繰り返す
             1. j-sum(j)を答えに加える
             2. add(a[j],1)
         sum(j)は i<j における a_i<= a_j となる数なので、 j-sum(j) で j を固定したときの転倒数が求まる
 
-    参考：http://hos.ac/slides/20140319_bit.pdf
-    verified: AOJ DSL_2_B
-        http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_B&lang=jp
+    応用: 集合の中で w 番目に小さいものや、ある要素が何番目に小さいかが高速に分かる
+        ・add(a,1): 集合への要素 a の追加(a 番目を 1 にする)
+        ・add(a,-1): 集合への要素 a の削除(a 番目を 1 から 0 にする)
+        ・sum(a): a が何番目(1はじまり)に小さいか
+        ・lower_bound(w): w 番目(1はじまり)に小さい要素 a は何か
+        値の範囲が N までとした時、メモリO(N), 計算量O(logN)だけかかる
+
+        verified: ARC033 C - データ構造
+            https://atcoder.jp/contests/arc033/tasks/arc033_3
 */
 
 #include <bits/stdc++.h>
@@ -17,52 +27,72 @@ using namespace std;
 
 /* BIT: 区間和の更新や計算を行う構造体
     初期値は a_1 = a_2 = ... = a_n = 0
-    ・add(i,a): a_i += a とする
+    ・add(i,x): a_i += x とする
     ・sum(i): a_1 + a_2 + ... + a_i を計算する
     ・query(l,r): [l,r) の区間の和を求める
+    ・lower_bound(w): a_1 + a_2 + ... + a_x >= w となるような最小の x を求める(ただし a_i >= 0)
     計算量は全て O(logn)
 */
 template <typename T>
 struct BIT {
-    int n;
-    vector<T> bit;
+    int n;          // 要素数
+    vector<T> bit;  // データの格納先
     BIT(int n_) : n(n_ + 1), bit(n, 0) {}
+
+    void add(int i, T x) {
+        if (i == 0) {
+            return;
+        } else {
+            for (int idx = i; idx <= n; idx += (idx & -idx)) {
+                bit[idx] += x;
+            }
+        }
+    }
 
     T sum(int i) {
         T s(0);
-        for (int x = i; x > 0; x -= (x & -x)) {
-            s += bit[x];
+        for (int idx = i; idx > 0; idx -= (idx & -idx)) {
+            s += bit[idx];
         }
         return s;
     }
 
-    void add(int i, T a) {
-        if (i == 0) {
-            return;
-        } else {
-            for (int x = i; x <= n; x += (x & -x)) {
-                bit[x] += a;
-            }
-        }
-    }
     // [l,r)
     T query(int l, int r) { return sum(r - 1) - sum(l - 1); }
+
+    int lower_bound(int w) {
+        if (w <= 0) {
+            return 0;
+        } else {
+            int x = 0, r = 1;
+            while (r < n) r = r << 1;
+            for (int len = r; len > 0; len = len >> 1) {
+                if (x + len <= n && bit[x + len] < w) {
+                    w -= bit[x + len];
+                    x += len;
+                }
+            }
+            return x + 1;
+        }
+    }
 };
 
 int main() {
-    int n, q;
-    cin >> n >> q;
-    BIT<int> bit(2e5);
-    vector<int> c(q), x(q), y(q);
+    int q;
+    cin >> q;
+    BIT<int> bit(3e5);
+    vector<int> T(q), X(q);
     for (int i = 0; i < q; i++) {
-        cin >> c.at(i) >> x.at(i) >> y.at(i);
+        cin >> T.at(i) >> X.at(i);
     }
 
     for (int i = 0; i < q; i++) {
-        if (c[i] == 0) {
-            bit.add(x[i], y[i]);
-        } else if (c[i] == 1) {
-            cout << bit.query(x[i], y[i] + 1) << endl;
+        if (T[i] == 1) {
+            bit.add(X[i], 1);
+        } else if (T[i] == 2) {
+            int a = bit.lower_bound(X[i]);
+            cout << a << endl;
+            bit.add(a, -1);
         }
     }
 
---
+++ data_structure/binary_indexed_tree_RAQ.cpp
@@ -0,0 +1,83 @@
+/* binary_indexed_tree_RAQ.cpp
+    BIT を用いた Range Add Query (RAQ)。区間への更新を行える
+
+    verified: AOJ DSL_2_E
+        http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_E&lang=ja
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* BIT: 区間和の更新や計算を行う構造体
+    初期値は a_1 = a_2 = ... = a_n = 0
+    ・add(l,r,x): [l,r) に x を加算する
+    ・sum(i): a_1 + a_2 + ... + a_i を計算する
+    ・query(l,r): [l,r) の区間の和を求める
+    計算量は全て O(logn)
+*/
+template <typename T>
+struct BIT {
+    int n;             // 要素数
+    vector<T> bit[2];  // データの格納先
+    BIT(int n_) : n(n_ + 1) { init(n); }
+    void init(int n) {
+        for (int p = 0; p < 2; p++) bit[p].assign(n, 0);
+    }
+
+    void add_sub(int p, int i, int x) {
+        if (i == 0) {
+            return;
+        } else {
+            for (int idx = i; idx <= n; idx += (idx & -idx)) {
+                bit[p][idx] += x;
+            }
+        }
+    }
+    void add(int l, int r, T x) {  // [l,r) に加算
+        add_sub(0, l, -x * (l - 1));
+        add_sub(0, r, x * (r - 1));
+        add_sub(1, l, x);
+        add_sub(1, r, -x);
+    }
+
+    T sum_sub(int p, int i) {
+        T s(0);
+        for (int idx = i; idx > 0; idx -= (idx & -idx)) {
+            s += bit[p][idx];
+        }
+        return s;
+    }
+    T sum(int i) { return sum_sub(0, i) + sum_sub(1, i) * i; }
+
+    // [l,r) の区間和を取得
+    T query(int l, int r) { return sum(r - 1) - sum(l - 1); }
+};
+
+int main() {
+    int n, q;
+    cin >> n >> q;
+    BIT<int> raq(2e5);
+
+    vector<int> ans;
+    for (int i = 0; i < q; i++) {
+        int c;
+        cin >> c;
+        if (c == 0) {
+            int s, t, x;
+            cin >> s >> t >> x;
+            raq.add(s, t + 1, x);
+        } else if (c == 1) {
+            int i;
+            cin >> i;
+            ans.push_back(raq.query(i, i + 1));
+        }
+    }
+    for (auto i : ans) {
+        cout << i << endl;
+    }
+
+    return 0;
+}
