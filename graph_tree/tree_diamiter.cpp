3228ae5b4bdade8c908ce28780127b272f87d1c7
unonao <naokiuno634@gmail.com>
2020-03-13 16:02:40 +0900

    use template

--- graph_tree/tree_diamiter.cpp
+++ graph_tree/tree_diamiter.cpp
@@ -8,27 +8,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <typename T>
 struct Edge {
     int to;
-    long long cost;
+    T cost;
 };
-using Graph = vector<vector<Edge>>;
+using Graph = vector<vector<Edge<long long>>>;  // cost の型を long long に指定
 
 /* tree_diamiter : dfs を用いて重み付き木 T の直径を求める
+    計算量: O(N)
 */
-pair<long long, int> dfs(const Graph &G, int u, int par) {
-    pair<long long, int> ret = make_pair(0LL, u);
+template <typename T>
+pair<T, int> dfs(const Graph &G, int u, int par) {  // 最遠点間距離と最遠点を求める
+    pair<T, int> ret = make_pair((T)0, u);
     for (auto e : G[u]) {
         if (e.to == par) continue;
-        auto next = dfs(G, e.to, u);
+        auto next = dfs<T>(G, e.to, u);
         next.first += e.cost;
         ret = max(ret, next);
     }
     return ret;
 }
-long long tree_diamiter(const Graph &G) {
-    auto p = dfs(G, 0, -1);
-    auto q = dfs(G, p.second, -1);
+template <typename T>
+T tree_diamiter(const Graph &G) {
+    pair<T, int> p = dfs<T>(G, 0, -1);
+    pair<T, int> q = dfs<T>(G, p.second, -1);
     return q.first;
 }
 
@@ -42,5 +46,5 @@ int main() {
         T[s].push_back({t, w});
         T[t].push_back({s, w});
     }
-    cout << tree_diamiter(T) << endl;
+    cout << tree_diamiter<long long>(T) << endl;
 }
