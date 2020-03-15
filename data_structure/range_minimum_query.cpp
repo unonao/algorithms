7a85e79a809358b7f5f93afad3a3f0e068767672
unonao <naokiuno634@gmail.com>
2020-03-15 19:11:57 +0900

    use lambda function

--- data_structure/range_minimum_query.cpp
+++ data_structure/range_minimum_query.cpp
@@ -19,10 +19,11 @@ using namespace std;
 */
 template <typename T>
 struct RMQ {
-    const T INF = numeric_limits<T>::max();
+    const T e = numeric_limits<T>::max();
+    function<T(T, T)> fx = [](T x1, T x2) -> T { return min(x1, x2); };
     int n;
     vector<T> dat;
-    RMQ(int n_) : n(), dat(n_ * 4, INF) {
+    RMQ(int n_) : n(), dat(n_ * 4, e) {
         int x = 1;
         while (n_ > x) {
             x *= 2;
@@ -32,7 +33,7 @@ struct RMQ {
 
     void set(int i, T x) { dat[i + n - 1] = x; }
     void build() {
-        for (int k = n - 2; k >= 0; k--) dat[k] = min(dat[2 * k + 1], dat[2 * k + 2]);
+        for (int k = n - 2; k >= 0; k--) dat[k] = fx(dat[2 * k + 1], dat[2 * k + 2]);
     }
 
     void update(int i, T x) {
@@ -40,7 +41,7 @@ struct RMQ {
         dat[i] = x;
         while (i > 0) {
             i = (i - 1) / 2;  // parent
-            dat[i] = min(dat[i * 2 + 1], dat[i * 2 + 2]);
+            dat[i] = fx(dat[i * 2 + 1], dat[i * 2 + 2]);
         }
     }
 
@@ -48,13 +49,13 @@ struct RMQ {
     T query(int a, int b) { return query_sub(a, b, 0, 0, n); }
     T query_sub(int a, int b, int k, int l, int r) {
         if (r <= a || b <= l) {
-            return INF;
+            return e;
         } else if (a <= l && r <= b) {
             return dat[k];
         } else {
             T vl = query_sub(a, b, k * 2 + 1, l, (l + r) / 2);
             T vr = query_sub(a, b, k * 2 + 2, (l + r) / 2, r);
-            return min(vl, vr);
+            return fx(vl, vr);
         }
     }
 
@@ -93,6 +94,7 @@ struct RMQ {
         cout << endl;
     }
 };
+
 int main() {
     int n, q;
     cin >> n >> q;
