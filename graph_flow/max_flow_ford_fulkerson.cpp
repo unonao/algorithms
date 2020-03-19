044800de1f71d921df8dd0df4d1379db35dc2145
unonao <naokiuno634@gmail.com>
2020-03-19 22:03:12 +0900

    modify

--- graph_flow/max_flow_ford_fulkerson.cpp
+++ graph_flow/max_flow_ford_fulkerson.cpp
@@ -14,11 +14,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-template <class FLOWTYPE>
+template <class T>
 struct Edge {
     int rev, from, to;  // rev:逆向きの辺の番号
-    FLOWTYPE cap, icap;
-    Edge(int r, int f, int t, FLOWTYPE c) : rev(r), from(f), to(t), cap(c), icap(c) {}
+    T cap, original_cap;
+    Edge(int r, int f, int t, T c) : rev(r), from(f), to(t), cap(c), original_cap(c) {}
     /*
     // debug
     friend ostream& operator<<(ostream& s, const Edge& E) {
@@ -28,45 +28,41 @@ struct Edge {
     */
 };
 
-template <class FLOWTYPE>
+template <class T>
 struct Graph {
-    vector<vector<Edge<FLOWTYPE>>> list;
-
-    Graph(int n = 0) : list(n) {}
+    vector<vector<Edge<T>>> G;
+    Graph(int n = 0) : G(n) {}
     void init(int n = 0) {
-        list.clear();
-        list.resize(n);
+        G.clear();
+        G.resize(n);
     }
     void reset() {
-        for (int i = 0; i < (int)list.size(); ++i)
-            for (int j = 0; j < list[i].size(); ++j) list[i][j].cap = list[i][j].icap;
+        for (int i = 0; i < (int)G.size(); i++)
+            for (int j = 0; j < G[i].size(); j++) G[i][j].cap = G[i][j].original_cap;
     }
-    inline vector<Edge<FLOWTYPE>>& operator[](int i) { return list[i]; }
-    inline const size_t size() const { return list.size(); }
+    vector<Edge<T>>& operator[](int i) { return G[i]; }
+    const size_t size() const { return G.size(); }
 
-    inline Edge<FLOWTYPE>& redge(Edge<FLOWTYPE> e) {  // 逆向きの辺を返す
-        if (e.from != e.to)
-            return list[e.to][e.rev];
-        else
-            return list[e.to][e.rev + 1];
+    Edge<T>& redge(Edge<T> e) {  // 逆向きの辺を返す
+        return G[e.to][e.rev];   // 自己ループはないと仮定（あれば G[e.to][e.rev + 1] とする必要がある）
     }
 
-    void add_edge(int from, int to, FLOWTYPE cap) {  // 辺を加える
-        list[from].push_back(Edge<FLOWTYPE>((int)list[to].size(), from, to, cap));
-        list[to].push_back(Edge<FLOWTYPE>((int)list[from].size() - 1, to, from, 0));
+    void add_edge(int from, int to, T cap) {  // 有向辺を加える
+        G[from].push_back(Edge<T>((int)G[to].size(), from, to, cap));
+        G[to].push_back(Edge<T>((int)G[from].size() - 1, to, from, 0));
     }
 
-    void add_undirected_edge(int from, int to, FLOWTYPE cap) {
-        list[from].push_back(Edge<FLOWTYPE>((int)list[to].size(), from, to, cap));
-        list[to].push_back(Edge<FLOWTYPE>((int)list[from].size() - 1, to, from, cap));
+    void add_undirected_edge(int from, int to, T cap) {  // 無向辺を加える
+        G[from].push_back(Edge<T>((int)G[to].size(), from, to, cap));
+        G[to].push_back(Edge<T>((int)G[from].size() - 1, to, from, cap));
     }
 
     /*
     // debug
     friend ostream& operator<<(ostream& s, const Graph& G) {
         s << endl;
-        for (int i = 0; i < G.list.size(); ++i) {
-            for (auto e : G.list[i]) {s << e << endl;}
+        for (int i = 0; i < G.G.size(); ++i) {
+            for (auto e : G.G[i]) {s << e << endl;}
         }return s;
     }*/
 };
@@ -74,20 +70,21 @@ struct Graph {
 /* FordFulkerson: Ford-Fulkersonのアルゴリズムで最大流を求める構造体
     Ford-Fulkerson ff;
     ff.max_flow(G,s,t) でグラフGの最大流が求まる
-    計算量: O(F|E|) (F:最大流の流量) (この最悪ケースになることはほぼ無い)
+    副作用：G は最大流の残余ネットワークになる
+    計算量: O(|f*||E|) (f*:最大流) (この最悪ケースになることはほぼ無い)
 */
-template <class FLOWTYPE>
+template <class T>
 struct FordFulkerson {
-    const FLOWTYPE INF = 1 << 30;
+    const T INF = 1e9;
     vector<int> used;
 
     FordFulkerson(){};
-    FLOWTYPE dfs(Graph<FLOWTYPE>& G, int v, int t, FLOWTYPE f) {
+    T dfs(Graph<T>& G, int v, int t, T f) {  // 増加可能経路を見つけて増加分のフローを返す
         if (v == t) return f;
         used[v] = true;
         for (auto& e : G[v]) {
             if (!used[e.to] && e.cap > 0) {
-                int d = dfs(G, e.to, t, min(f, e.cap));
+                T d = dfs(G, e.to, t, min(f, e.cap));
                 if (d > 0) {
                     e.cap -= d;
                     G.redge(e).cap += d;
@@ -97,11 +94,11 @@ struct FordFulkerson {
         }
         return 0;
     }
-    FLOWTYPE max_flow(Graph<FLOWTYPE>& G, int s, int t) {
-        int flow = 0;
+    T max_flow(Graph<T>& G, int s, int t) {
+        T flow = 0;
         while (true) {
             used.assign(G.size(), 0);
-            int f = dfs(G, s, t, INF);
+            T f = dfs(G, s, t, INF);  // f が増加分のフロー
             if (f == 0) {
                 return flow;
             } else {
