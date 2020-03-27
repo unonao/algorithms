d9d354a77ba406ff936171676a046d6c75dd6df4
unonao <naokiuno634@gmail.com>
2020-03-27 21:53:56 +0900

    change val name

--- string/trie_tree.cpp
+++ string/trie_tree.cpp
@@ -1,5 +1,5 @@
 /* trie_tree.cpp
-    trie 木（prefix 関係の構造体。文字列検索を高速にできる）
+    trie 木（prefix 関係の構造体。単語検索を高速にできる）
 
     verify: Google Kick Start 2020 Round A  Bundling (14pts, 21pts)
         https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ffc7/00000000001d3ff3
@@ -9,20 +9,21 @@
 using namespace std;
 
 /* Trie 木： 文字の種類(char_size)、int型で0に対応する文字(base)
-    insert(str): 文字列 str を Trie 木に挿入する
-    search(str): 文字列 str が Trie 木にあるか判定する
-    start_with(prefix):  prefix が一致する文字列が Trie 木にあるか判定する
-    count(): 挿入した文字列の数を返す
+    insert(word): 単語 word を Trie 木に挿入する
+    search(word): 単語 word が Trie 木にあるか判定する
+    start_with(prefix):  prefix が一致する単語が Trie 木にあるか判定する
+    count(): 挿入した単語の数を返す
     size(): Trie 木の頂点数を返す
-    計算量：inset, search ともに O(M)（Mは文字列の長さ）
+    計算量：insert, search ともに O(M)（Mは単語の長さ）
 */
+
 template <int char_size, int base>
 struct Trie {
-    struct Node {
+    struct Node {            // 頂点を表す構造体
         vector<int> next;    // 子の頂点番号を格納。存在しなければ-1
-        vector<int> accept;  // 末端がこの頂点になる文字列の str_id を保存
+        vector<int> accept;  // 末端がこの頂点になる単語の word_id を保存
         int c;               // base からの間隔をint型で表現したもの
-        int common;          // いくつの文字列がこの頂点を共有しているか
+        int common;          // いくつの単語がこの頂点を共有しているか
         Node(int c_) : c(c_), common(0) {
             next.assign(char_size, -1);
         }
@@ -34,11 +35,11 @@ struct Trie {
         nodes.push_back(Node(root));
     }
 
-    // 文字列の挿入
-    void insert(const string &str, int str_id) {
+    // 単語の挿入
+    void insert(const string &word, int word_id) {  // 単語:str と 単語の番号: str_id
         int node_id = 0;
-        for (int i = 0; i < (int)str.size(); i++) {
-            int c = (int)(str[i] - base);
+        for (int i = 0; i < (int)word.size(); i++) {
+            int c = (int)(word[i] - base);
             int &next_id = nodes[node_id].next[c];
             if (next_id == -1) {  // 次の頂点が存在しなければ追加
                 next_id = (int)nodes.size();
@@ -48,32 +49,32 @@ struct Trie {
             node_id = next_id;
         }
         ++nodes[node_id].common;
-        nodes[node_id].accept.push_back(str_id);
+        nodes[node_id].accept.push_back(word_id);  // 単語の終端なので、頂点に番号を入れておく
     }
-    void insert(const string &str) {
-        insert(str, nodes[0].common);
+    void insert(const string &word) {
+        insert(word, nodes[0].common);
     }
 
-    // 文字列の検索
-    bool search(const string &str, bool prefix = false) {
+    // 単語とprefixの検索
+    bool search(const string &word, bool prefix = false) {
         int node_id = 0;
-        for (int i = 0; i < (int)str.size(); i++) {
-            int c = (int)(str[i] - base);
+        for (int i = 0; i < (int)word.size(); i++) {
+            int c = (int)(word[i] - base);
             int &next_id = nodes[node_id].next[c];
             if (next_id == -1) {  // 次の頂点が存在しなければ終了
                 return false;
             }
             node_id = next_id;
         }
-        return (prefix) ? true : nodes[node_id].accept.size() > 0;
+        return (prefix) ? true : nodes[node_id].accept.size() > 0;  // 最後の頂点が受理状態か確認
     }
 
-    // str が prefix となる文字列が存在するかの検索
+    // prefix を持つ単語が存在するかの検索
     bool start_with(const string &prefix) {
         return search(prefix, true);
     }
 
-    // 挿入した文字列の数
+    // 挿入した単語の数
     int count() const {
         return (nodes[0].common);
     }
@@ -81,7 +82,6 @@ struct Trie {
     int size() const {
         return ((int)nodes.size());
     }
-
     /*
     // 文字列Sのプレフィックスに一致する文字列を検索する。一致した文字列ごとに関数fが呼び出される。
     void query(const string &str, const function<void(int)> &f, int str_index, int node_id) {
