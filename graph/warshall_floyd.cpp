f4d244233c3a1d725f20b3450c50d6aa249ebcad
unonao <naokiuno634@gmail.com>
2020-03-30 10:59:04 +0900

    chang val name

--- graph/warshall_floyd.cpp
+++ graph/warshall_floyd.cpp
@@ -2,7 +2,7 @@
     ワーシャルフロイド法を用いて、全点間最短路を求める。
 
     初期化：dis[i][j] = iからjへの距離(辺がなければINF) (i≠j)
-            dis[i][i] = 0
+            dist[i][i] = 0
             prev[i][j] = i
 
     負閉路の検出：dis[i][i] < 0 となる i が存在するか
@@ -15,18 +15,18 @@
 using namespace std;
 const long long INF = 1e17;
 
-/* warshall_floyd(dis,prev)
-    入力：初期化した dis, prev
+/* warshall_floyd(dist,prev)
+    入力：初期化した dist, prev
     計算量：O(|V|^3)
     副作用：dis[i][j]にiからjへの最短路のコストを格納、prev[i][j]にiからjへの最短路でのjの1つ前の点を格納
 */
-void warshall_floyd(vector<vector<long long>> &dis, vector<vector<long long>> &prev) {
-    int V = dis.size();
+void warshall_floyd(vector<vector<long long>> &dist, vector<vector<long long>> &prev) {
+    int V = dist.size();
     for (int k = 0; k < V; k++) {
         for (int i = 0; i < V; i++) {
             for (int j = 0; j < V; j++) {
-                if (dis[i][k] + dis[k][j] < dis[i][j]) {
-                    dis[i][j] = dis[i][k] + dis[k][j];
+                if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
                     prev[i][j] = prev[k][j];
                 }
             }
@@ -48,14 +48,14 @@ vector<int> get_path(const vector<vector<long long>> &prev, int s, int t) {
     return path;
 }
 
-/* is_negative(dis)
-    入力：warshall_floyd で得た dis
+/* is_negative(dist)
+    入力：warshall_floyd で得た dist
     出力：負の有向路を含むかどうか
 */
-bool is_negative(const vector<vector<long long>> &dis) {
-    int V = dis.size();
+bool is_negative(const vector<vector<long long>> &dist) {
+    int V = dist.size();
     for (int i = 0; i < V; i++) {
-        if (dis[i][i] < 0) {
+        if (dist[i][i] < 0) {
             return true;
         }
     }
@@ -67,10 +67,10 @@ int main() {
     cin >> V >> E;
 
     // initialization
-    vector<vector<long long>> dis(V, vector<long long>(V, INF));
+    vector<vector<long long>> dist(V, vector<long long>(V, INF));
     vector<vector<long long>> prev(V);
     for (int i = 0; i < V; ++i) {
-        dis[i][i] = 0;
+        dist[i][i] = 0;
         prev[i] = vector<long long>(V, i);
     }
 
@@ -78,21 +78,21 @@ int main() {
     for (int i = 0; i < E; i++) {
         long long s, t, c;
         cin >> s >> t >> c;
-        dis[s][t] = c;
-        // dis[t][s] = c;
+        dist[s][t] = c;
+        // dist[t][s] = c;
     }
 
-    warshall_floyd(dis, prev);
+    warshall_floyd(dist, prev);
 
-    if (is_negative(dis)) {
+    if (is_negative(dist)) {
         cout << "NEGATIVE CYCLE" << endl;
     } else {
         for (int i = 0; i < V; i++) {
             for (int j = 0; j < V; j++) {
-                if (dis[i][j] >= INF / 2) {
+                if (dist[i][j] >= INF / 2) {
                     cout << "INF";
                 } else {
-                    cout << dis[i][j];
+                    cout << dist[i][j];
                 }
                 if (j != V - 1) {
                     cout << " ";
