13c63be90ea64ceefeb8c7d6f9c6b11a10679222
unonao <naokiuno634@gmail.com>
2020-04-01 10:32:20 +0900

    use unit and verify

--- dynamic_programming/rerooting.cpp
+++ dynamic_programming/rerooting.cpp
@@ -1,46 +1,58 @@
 /* rerooting.cpp
     全方位木DP
 
-// 一辺コスト1の木について、各頂点から最も遠い点までの距離
-    struct DP {
+    // 根rから最も遠い点までの距離を求める
+    struct DP {  // 単位元はしっかり定義する(末端でもfrされるので注意)
         long long dp;
-        DP(long long dp_ = -1) : dp(dp_) {}  // 頂点一つ(子が0)の部分木の時は-1,
+        DP(long long dp_ = 0) : dp(dp_) {}
     };
-    function<DP(DP, DP)> fd = [](DP d1, DP d2) -> DP {  // dp 同士の二項演算
-        return DP(max(d1.dp, d2.dp));                   //最大の深さ(根から最も遠い点までの距離)
+    function<DP(DP, DP, long long)> fd = [](DP dp_cum, DP d, long long cost) -> DP {  // d:辺eに対応する部分木のdpの値  cost:eのコスト
+        return DP(max(dp_cum.dp, d.dp + cost));                                       //最大の深さ(根から最も遠い点までの距離)
     };
     function<DP(DP)> fr = [](DP d) -> DP {  // まとめたDPを用いて、新たな部分木のDPを計算する
-        return DP(d.dp + 1);
+        return d;
     };
 
-    verified: AOJ Traffic Tree
+    verified:
+    - AOJ GRL_5_A: Diameter of a Tree
+        http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_5_A
+    - [AtCoder] Typical DP Contest N - 木
+        https://tdpc.contest.atcoder.jp/submissions/11405065
+    - [AtCoder] ABC 160 F - Distributing Integers （上とほぼ同じ）
+        https://atcoder.jp/contests/abc160/submissions/11405183
+
+    - AOJ 1595: Traffic Tree
         http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1595
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-/* Rerooting:
+/* Rerooting: 全方位木 DP
     問題ごとに以下を書き換える
-    - 型DP
+    - 型DPと単位元
     - 型DPに対する二項演算 fd
     - まとめたDPを用いて新たな部分木のDPを計算する fr
     計算量: O(N)
 */
 struct Rerooting {
-    // DP と演算の定義（問題ごとに書き換える）
-    struct DP {
+    /* start 問題ごとに書き換え */
+    struct DP {  // DP の型
         long long dp;
-        DP(long long dp_ = -1) : dp(dp_) {}  // 単位元はしっかり定義する(末端でもfrされるので注意)
+        DP(long long dp_) : dp(dp_) {}
     };
-    function<DP(DP, DP)> fd = [](DP d1, DP d2) -> DP {  // dp 同士の二項演算
-        return DP(max(d1.dp, d2.dp));                   //最大の深さ(根から最も遠い点までの距離)
+    const DP unit_dp = DP(0);                                                         // 単位元はしっかり定義する(末端でもfrされるので注意)
+    function<DP(DP, DP, long long)> fd = [](DP dp_cum, DP d, long long cost) -> DP {  // d:辺eに対応する部分木のdpの値  cost:eのコスト
+        return DP(max(dp_cum.dp, d.dp + cost));
     };
     function<DP(DP)> fr = [](DP d) -> DP {  // まとめたDPを用いて、新たな部分木のDPを計算する
-        return DP(d.dp + 1);
+        return d;
     };
+    /* end 問題ごとに書き換え */
 
+    // グラフの定義
     struct Edge {
+        int from;
         int to;
         long long cost;
     };
@@ -50,46 +62,49 @@ struct Rerooting {
     vector<DP> ans;         // ans[v]: 頂点vを根とする木の答え
     Graph G;
 
-    Rerooting(int N) : dp(N), ans(N), G(N) {}
+    Rerooting(int N) : G(N) {
+        dp.resize(N);
+        ans.assign(N, unit_dp);
+    }
 
     void add_edge(int a, int b, long long c = 1) {
-        G[a].push_back({b, c});
+        G[a].push_back({a, b, c});
     }
     void build() {
-        dfs(0);  // 普通に木DP
-        bfs(0);  // 残りの部分木に対応するDPを計算
+        dfs(0);           // 普通に木DP
+        bfs(0, unit_dp);  // 残りの部分木に対応するDPを計算
     }
 
     DP dfs(int v, int p = -1) {  // 頂点v, 親p
-        DP dp_cum;
+        DP dp_cum = unit_dp;
         int deg = G[v].size();
-        dp[v] = vector<DP>(deg);
+        dp[v] = vector<DP>(deg, unit_dp);
         for (int i = 0; i < deg; i++) {
             int u = G[v][i].to;
             if (u == p) continue;
             dp[v][i] = dfs(u, v);
-            dp_cum = fd(dp_cum, dp[v][i]);
+            dp_cum = fd(dp_cum, dp[v][i], G[v][i].cost);
         }
         return fr(dp_cum);
     }
-    void bfs(int v, const DP& dp_p = DP(), int p = -1) {
+    void bfs(int v, const DP& dp_p, int p = -1) {
         int deg = G[v].size();
         for (int i = 0; i < deg; i++) {  // 前のbfsで計算した有向辺に対応する部分木のDPを保存
             if (G[v][i].to == p) dp[v][i] = dp_p;
         }
 
-        vector<DP> dp_l(deg + 1), dp_r(deg + 1);  // 累積的なDP
+        vector<DP> dp_l(deg + 1, unit_dp), dp_r(deg + 1, unit_dp);  // 累積的なDP
         for (int i = 0; i < deg; i++) {
-            dp_l[i + 1] = fd(dp_l[i], dp[v][i]);
+            dp_l[i + 1] = fd(dp_l[i], dp[v][i], G[v][i].cost);
         }
         for (int i = deg - 1; i >= 0; i--) {
-            dp_r[i] = fd(dp_r[i + 1], dp[v][i]);
+            dp_r[i] = fd(dp_r[i + 1], dp[v][i], G[v][i].cost);
         }
         ans[v] = fr(dp_l[deg]);
         for (int i = 0; i < deg; i++) {
             int u = G[v][i].to;
             if (u == p) continue;
-            bfs(u, fr(fd(dp_l[i], dp_r[i + 1])), v);
+            bfs(u, fr(fd(dp_l[i], dp_r[i + 1], 0)), v);  // 累積同士のfdなので、edgeは適当に
         }
     }
 };
@@ -106,7 +121,8 @@ int main() {
         reroot.add_edge(v, u);
     }
     reroot.build();
+
     for (int i = 0; i < N; i++) {
-        cout << (N - 1) * 2 - reroot.ans[i].dp << "\n";
+        cout << (N - 1) * 2 - reroot.ans[i].dp << endl;
     }
 }
